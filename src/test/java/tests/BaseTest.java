7e2d9f0def1d3a3105c6d63fa021f9996e9fd5f4
Maria <mfkaredina@gmail.com>
2022-07-18 22:08:08 +0300

    One BaseTest for two classes added

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,14 +1,12 @@
 package tests;
 
-import constants.Urls;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.PropertyManager;
 
-public class BaseTestDataTable {
+public class BaseTest {
     WebDriver driver;
 
     @BeforeMethod
@@ -17,10 +15,12 @@ public class BaseTestDataTable {
         propertyManager.loadData();
         System.setProperty("webdriver.chrome.driver", propertyManager.get("PATH_TO_DRIVER"));
         driver = new ChromeDriver();
-        driver.get(Urls.DATA_TABLE_URL);
+
     }
+
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
         driver.quit();
     }
+
 }
--- src/test/java/tests/DataTables.java
+++ src/test/java/tests/DataTables.java
@@ -1,12 +1,14 @@
 package tests;
 
+import constants.Urls;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class DataTables extends BaseTestDataTable {
+public class DataTables extends BaseTest {
     @Test
     public void firstNameAndLastNameTest() {
+        driver.get(Urls.DATA_TABLE_URL);
         String firstName = driver.findElement(By.xpath("//table[@id='table1']//td[text()='fbach@yahoo.com']//preceding-sibling::td[1]")).getText();
 
         String lastName = driver.findElement(By.xpath("//table[@id='table1']//td[text()='fbach@yahoo.com']//preceding-sibling::td[2]")).getText();
@@ -17,6 +19,7 @@ public class DataTables extends BaseTestDataTable {
 
     @Test
     public void dueAndWebsiteTest() {
+        driver.get(Urls.DATA_TABLE_URL);
         String due = driver.findElement(By.xpath("//table[@id='table2']//td[text()='fbach@yahoo.com']//following-sibling::td[@class='dues']")).getText();
 
         String webSite = driver.findElement(By.xpath("//table[@id='table2']//td[text()='fbach@yahoo.com']//following-sibling::td[@class='web-site']")).getText();
